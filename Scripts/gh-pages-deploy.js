9a0bb912f84cc7011d0db0cb8ea7c6226cbbf87a
Haoping Xu <haoping.xu@mail.utoronto.ca>
2021-09-21 15:39:51 -0400

    change deploy script and point it ot website branch

--- Scripts/gh-pages-deploy.js
+++ Scripts/gh-pages-deploy.js
@@ -14,7 +14,7 @@ const fs = require("fs");
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-    await execa("git", ["checkout", "-f", "main"]);
+    await execa("git", ["checkout", "-f", "website"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed, check your settings");
   } catch (e) {
